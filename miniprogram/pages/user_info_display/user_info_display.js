499ee20f6c6dc03e784253b7aad5ca7ab0f29372
Shengkang Chen <billyskc@ucla.edu>
2019-09-28 16:21:33 -0400

    able to pull user info from database

--- miniprogram/pages/user_info_display/user_info_display.js
+++ miniprogram/pages/user_info_display/user_info_display.js
@@ -1,7 +1,21 @@
-Page({  
-  data:{
-    name: "user name"
+//https://cloud.tencent.com/developer/article/1380912
 
-  }
+const db = wx.cloud.database({});
+const cont = db.collection('user');
+var temp;
+
+Page({
+  data: {
+    name: temp
+  },
+  onLoad: function (options) {
+    var _this = this;
+    db.collection('user').get({
+      success(res) {
+        console.log(res.data);
+        temp = res.data.age;
+      }
+    })
+  },
 
 })
